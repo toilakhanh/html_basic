914356299c9ea47cf5a122ce71b44e15f6faac62
Tuananh2222 <vut5441@gmail.com>
2022-06-10 21:46:31 +0700

    update JS

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,5 @@
 document.getElementById("btnCV").onclick = ()=>{
  
-  document.getElementById("linkCV").setAttribute("href","https://www.topcv.vn/xem-cv/UgAAXQUGBAdVBFIFDlNSVwdVBwFRAFQABFRRVQd18d")
+  document.getElementById("btnCV").setAttribute("href","https://www.topcv.vn/xem-cv/UgAAXQUGBAdVBFIFDlNSVwdVBwFRAFQABFRRVQd18d")
 }
 
